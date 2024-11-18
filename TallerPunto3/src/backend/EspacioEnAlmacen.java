342332c3300f0cf583bfacb93c25f7affea0e3da
AngelDavidGutierrez <angelgutierrez1132998@correo.itm.edu.co>
2024-11-17 23:05:43 -0500

    Agregando Codigo

--- TallerPunto3/src/backend/EspacioEnAlmacen.java
+++ TallerPunto3/src/backend/EspacioEnAlmacen.java
@@ -1,7 +1,1 @@
-package backend;
 
-public class EspacioEnAlmacen extends PuntoDeVenta {
-    public EspacioEnAlmacen(String codigo, String direccion, String telefono, double area) {
-        super(codigo, direccion, telefono, area);
-    }
-}
