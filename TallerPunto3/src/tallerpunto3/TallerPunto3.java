dd29eafd30d0361bc70eac270b66d4ddf4fc0bb1
AngelDavidGutierrez <angelgutierrez1132998@correo.itm.edu.co>
2024-11-20 17:36:30 -0500

    Update TallerPunto3.java

--- TallerPunto3/src/tallerpunto3/TallerPunto3.java
+++ TallerPunto3/src/tallerpunto3/TallerPunto3.java
@@ -26,7 +26,48 @@ public class TallerPunto3 {
             
             switch (opcion) {
                 case 1:
-                    System.out.println("");
+                    System.out.println("Seleccione el tipo de punto de venta:");
+                    System.out.println("1. Punto de Fabrica");
+                    System.out.println("2. Tienda en Centro Comercial");
+                    System.out.println("3. Espacio en Almacen");
+                    int tipo = scanner.nextInt();
+                    scanner.nextLine();
+                    
+                    System.out.println("Ingrese el codigo:");
+                    String codigo = scanner.nextLine();
+                    System.out.println("Ingrese la direccion:");
+                    String direccion = scanner.nextLine();
+                    System.out.println("Ingrese el telefono:");
+                    String telefono = scanner.nextLine();
+                    System.out.println("Ingrese el area:");
+                    double area = scanner.nextDouble();
+                    scanner.nextLine();
+                    
+                    switch (tipo) {
+                        case 1: // Ingresar punto de venta
+                            System.out.println("Ingrese el administrador:");
+                            String administradorF = scanner.nextLine(); // la F es de Fabrica
+                            System.out.println("Ingrese el numero de empleados:");
+                            int empleadosF = scanner.nextInt();
+                            scanner.nextLine();
+                            puntosDeVenta.add(new PuntoFabrica(codigo, direccion, telefono, area, administradorF, empleadosF));
+                            break;
+                        case 2:
+                            System.out.println("Ingrese el administrador");
+                            String administradorT = scanner.nextLine(); // la T es de Tienda
+                            System.out.println("Ingrese el numero de empleados:");
+                            int empleadosT = scanner.nextInt();
+                            scanner.nextLine();
+                            System.out.println("Ingrese la linea de ropa mas vendida:");
+                            String lineaRopa = scanner.nextLine();
+                            puntosDeVenta.add(new TiendaCentroComercial(codigo, direccion, telefono, area, administradorT, empleadosT, lineaRopa));
+                            break;
+                        case 3:
+                            puntosDeVenta.add(new EspacioAlmacen(codigo, direccion, telefono, area));
+                            break;
+                        default:
+                            System.out.println("Tipo no valido");
+                    }
                     break;
                 case 2:
                     System.out.println("");
