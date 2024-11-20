165ddee3273a7d27b9dbc520dd7572bba9da8d31
AngelDavidGutierrez <angelgutierrez1132998@correo.itm.edu.co>
2024-11-19 21:48:58 -0500

    añadiendo TiendaCentroComercial.java

--- TallerPunto3/src/backend/TiendaCentroComercial.java
+++ TallerPunto3/src/backend/TiendaCentroComercial.java
@@ -1,1 +1,22 @@
+package backend;
 
+public class TiendaCentroComercial extends PuntoVenta {
+    private String administrador;
+    private int numeroEmpleados;
+    private String lineaRopaMasVendida;
+    
+    public TiendaCentroComercial(String codigo, String direccion, String telefono, double area, String administrador, int numeroEmpleados, String lineaRopaMasVendida) {
+        super(codigo, direccion, telefono, area);
+        this.administrador = administrador;
+        this.numeroEmpleados = numeroEmpleados;
+        this.lineaRopaMasVendida = lineaRopaMasVendida;
+    }
+    
+    @Override
+    public void consultar() {
+        super.consultar();
+        System.out.println("Administrador: " + administrador);
+        System.out.println("Número de empleados: " + numeroEmpleados);
+        System.out.println("Línea de ropa más vendida: " + lineaRopaMasVendida);
+    }
+}
